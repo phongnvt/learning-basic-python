99e37809ca8c988798c587d1c7a7858240d0a6a1
phongnvt <thanhphongblaster@gmail.com>
2023-07-12 22:38:12 +0700

    Nhap xuat, xu ly chuoi don gian

---
+++ nhap-xuat-co-ban/buoi2.py
@@ -0,0 +1,32 @@
+import datetime
+'''nam_sinh = int(input("Bạn sinh năm bao nhiêu? "))
+tuoi = datetime.datetime.now().year - nam_sinh
+print(f"Nếu bạn sinh năm {nam_sinh} thì số tuổi của bạn là {tuoi} tuổi")
+print("Nếu bạn sinh năm {1} thì số tuổi của bạn là {0} tuổi".format(nam_sinh, tuoi))
+
+
+so_thu_nhat = int(input("Nhập vào số thứ nhất: "))
+so_thu_hai = int(input("Nhập vào số thứ hai: "))
+
+tong = so_thu_nhat + so_thu_hai
+hieu = so_thu_nhat - so_thu_hai
+
+print("Kết quả là:")
+print(f"{so_thu_nhat} + {so_thu_hai} = {tong}")
+print("{2} - {1} = {0}".format(hieu,so_thu_hai,so_thu_nhat))
+'''
+ten = "thANH phONG"
+'''
+print(ten.capitalize())
+
+print(ten.upper())
+
+print(ten.lower())
+
+print(f"Tên bạn đã nhập có {ten.__len__()} kí tự")
+
+print(ten.endswith("ng"))'''
+print(ten[6:9])
+
+# import 
+
